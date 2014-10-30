39ce102ad9a664a82372d5accc8246f080c2e949
amaro <amaro@lepton.cc.gt.atl.ga.us>
2014-10-29 22:49:44 -0400

    call hypertest hypercall from hello sys call

--- amaro/amaro.c
+++ amaro/amaro.c
@@ -1,8 +1,11 @@
 #include <linux/kernel.h>
 #include <linux/syscalls.h>
+#include <asm/xen/hypercall.h>
 
 asmlinkage long sys_hello(void)
 {
     printk("Hello World sys_hello\n");
+    HYPERVISOR_hypertest(0);
+    printk("hypercall executed\n");
     return 0;
 }
